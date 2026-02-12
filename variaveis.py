3f30032270a0b39c55aac95d5a6f61a84b8345ac
Adones Melo <adones.n.m@outlook.com>
2026-02-11 21:00:49 -0300

    Questão 2: Variáveis

--- variaveis.py
+++ variaveis.py
@@ -5,4 +5,16 @@ Percentual_bonus = 0.1
 bonus_total = Faturamento_inicial * Percentual_bonus
 faturamento_liquido = Faturamento_inicial - bonus_total
 
-print(f'Bonus_total: R${bonus_total:,.2f} | Faturamento líquido: R${faturamento_liquido:,.2f}')
+print(f'\nBonus_total: R${bonus_total:,.2f} | Faturamento líquido: R${faturamento_liquido:,.2f}\n')
+
+print('#######################################################################################')
+
+# 2
+estoque = 250
+vendidos = 78
+reposicao = 100
+
+estoque = estoque - vendidos + reposicao
+print(f'\nEstoque atualizado: {estoque} Un.\n')
+
+print('#######################################################################################')

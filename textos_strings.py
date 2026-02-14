c059e3c62b4f0ac878ec7ac9ce77fea0622bb568
Adones Melo <adones.n.m@outlook.com>
2026-02-14 18:22:53 -0300

    Questão 2: Texto e Strings

--- textos_strings.py
+++ textos_strings.py
@@ -9,3 +9,13 @@ print('\n#######################################################################
 print(f'Lucro: R${lucro:,.2f} | margem: {margem:.0%}')
 
 print('\n#######################################################################################\n')
+
+# 2
+nome = ' mArCoS aNtOnIo rOcHa '
+email = ' MARCOS.ROCHA@GMAIL.COM '
+
+nome = nome.strip().title()
+print(nome)
+
+email = email.strip().lower()
+print(email)

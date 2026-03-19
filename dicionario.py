9585058de017940ac2e36faae77c45d9492e262e
Adones Melo <adones.n.m@outlook.com>
2026-03-19 19:50:00 -0300

    Questão 1: Dicionários

---
+++ dicionario.py
@@ -0,0 +1,15 @@
+print('\n#######################################################################################\n')
+
+# 1
+clientes = {'Lira': 5000, 'Alon': 3000, 'Julia': 4500}
+
+nova_compra = 1500
+clientes['Alon'] = clientes['Alon'] + nova_compra
+
+# novo cliente
+clientes['Marcos'] = 2000
+
+# lista de clientes atualizada
+print(clientes)
+
+print('\n#######################################################################################\n')

e4c24bb9c321e6faee6751090a4bf5708b8a5ab1
Adones Melo <adones.n.m@outlook.com>
2026-03-19 20:19:49 -0300

    Questão 3: Dicionários

--- dicionario.py
+++ dicionario.py
@@ -1,24 +1,39 @@
+# print('\n#######################################################################################\n')
+
+# # 1
+# clientes = {'Lira': 5000, 'Alon': 3000, 'Julia': 4500}
+
+# nova_compra = 1500
+# clientes['Alon'] = clientes['Alon'] + nova_compra
+
+# # novo cliente
+# clientes['Marcos'] = 2000
+
+# # lista de clientes atualizada
+# print(clientes)
+
+# print('\n#######################################################################################\n')
+
+# # 2
+# estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
+# produto = input('Digite o nome do produto: ').strip().lower()
+
+# if produto in estoque:
+#     print(f'O produto {produto} tem {estoque[produto]} unidades no estoque.')
+# else:
+#     print('Produto não encontrado no sistema')
+
 print('\n#######################################################################################\n')
 
-# 1
-clientes = {'Lira': 5000, 'Alon': 3000, 'Julia': 4500}
+# 3
+vendas_regiao = {"Norte": 15000, "Sul": 22000, "Leste": 18000, "Oeste": 25000}
 
-nova_compra = 1500
-clientes['Alon'] = clientes['Alon'] + nova_compra
+# somentes os valores
+lista_vendas = list(vendas_regiao.values())
+print(lista_vendas)
 
-# novo cliente
-clientes['Marcos'] = 2000
+fat_total = sum(lista_vendas)
+print(f'Faturamento total: R$ {fat_total:,.2f}')
 
-# lista de clientes atualizada
-print(clientes)
-
-print('\n#######################################################################################\n')
-
-# 2
-estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
-produto = input('Digite o nome do produto: ').strip().lower()
-
-if produto in estoque:
-    print(f'O produto {produto} tem {estoque[produto]} unidades no estoque.')
-else:
-    print('Produto não encontrado no sistema')
+media_fat = fat_total / len(lista_vendas)
+print(f'Média de faturamento: R$ {media_fat:,.2f}')

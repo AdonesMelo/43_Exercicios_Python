188a6ec2f074b2bf08a13c694b9b3a2284b3f287
Adones Melo <adones.n.m@outlook.com>
2026-03-28 20:55:26 -0300

    Questão 4: Dicionários

--- dicionario.py
+++ dicionario.py
@@ -1,27 +1,27 @@
-# print('\n#######################################################################################\n')
+print('\n#######################################################################################\n')
 
-# # 1
-# clientes = {'Lira': 5000, 'Alon': 3000, 'Julia': 4500}
+# 1
+clientes = {'Lira': 5000, 'Alon': 3000, 'Julia': 4500}
 
-# nova_compra = 1500
-# clientes['Alon'] = clientes['Alon'] + nova_compra
+nova_compra = 1500
+clientes['Alon'] = clientes['Alon'] + nova_compra
 
-# # novo cliente
-# clientes['Marcos'] = 2000
+# novo cliente
+clientes['Marcos'] = 2000
 
-# # lista de clientes atualizada
-# print(clientes)
+# lista de clientes atualizada
+print(clientes)
 
-# print('\n#######################################################################################\n')
+print('\n#######################################################################################\n')
 
-# # 2
-# estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
-# produto = input('Digite o nome do produto: ').strip().lower()
+# 2
+estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
+produto = input('Digite o nome do produto: ').strip().lower()
 
-# if produto in estoque:
-#     print(f'O produto {produto} tem {estoque[produto]} unidades no estoque.')
-# else:
-#     print('Produto não encontrado no sistema')
+if produto in estoque:
+    print(f'O produto {produto} tem {estoque[produto]} unidades no estoque.')
+else:
+    print('Produto não encontrado no sistema')
 
 print('\n#######################################################################################\n')
 
@@ -37,3 +37,15 @@ print(f'Faturamento total: R$ {fat_total:,.2f}')
 
 media_fat = fat_total / len(lista_vendas)
 print(f'Média de faturamento: R$ {media_fat:,.2f}')
+
+print('\n#######################################################################################\n')
+
+# 4
+desempenho = {"Lira": [8, 9, 7], "Paula": [10, 9, 10], "Tiago": [6, 7, 8]}
+nome = input('Digite o nome do colaborador: ')
+print(f'Nome: {nome}')
+
+notas = desempenho[nome]
+print(f'Notas: {notas}')
+media = sum(notas) / len(notas)
+print(f'Media: {media}')

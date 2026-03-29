f5d373234b82931e6aa34b515a052aaa0176112d
Adones Melo <adones.n.m@outlook.com>
2026-03-29 20:24:03 -0300

    Questão 2: Estrutura de Repetição

--- estrutura_repeticao.py
+++ estrutura_repeticao.py
@@ -6,3 +6,19 @@ for i in range(10):
     print(f'Lembrete: O treinamento de Python começa em {tempo_regressivo} minutos.')
 
 print('\n#######################################################################################\n')
+
+# 2
+vendas = [2000, 5000, 1000, 8000, 3000]
+
+comissao_total = 0 
+for venda in vendas:
+    if venda > 4000:
+        comissao = 0.1
+    else:
+        comissao = 0.05
+    
+    comissao_total = comissao_total + comissao * venda
+
+print(f'O vendedor receberá R${comissao_total:,.2f}')
+
+print('\n#######################################################################################\n')

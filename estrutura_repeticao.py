4c13699204ad0478580777faef66a03fb48d5a4e
Adones Melo <adones.n.m@outlook.com>
2026-03-29 20:22:00 -0300

    Questão 1: Estrutura de Repetição

---
+++ estrutura_repeticao.py
@@ -0,0 +1,8 @@
+print('\n#######################################################################################\n')
+
+# 1
+for i in range(10):
+    tempo_regressivo = 10 - i
+    print(f'Lembrete: O treinamento de Python começa em {tempo_regressivo} minutos.')
+
+print('\n#######################################################################################\n')

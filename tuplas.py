e38126800289a4e25193f878a13a5e41bbd97e63
Adones Melo <adones.n.m@outlook.com>
2026-06-19 13:24:53 -0300

    Questão 4: Tuplas

--- tuplas.py
+++ tuplas.py
@@ -27,3 +27,19 @@ vendas_dia = [("Monitor", 900, 2), ("Teclado", 150, 5), ("Mouse", 80, 10)]
 # Iterando sobre a lista de vendas
 for produto, preco_unitario, quantidade in vendas_dia: # desempacotamento de tupla
     print(f'Produto: {produto:<10} | Total: R${preco_unitario * quantidade:.2f}')
+
+print('\n#######################################################################################\n')
+# 4
+dados_filiais = {"Matriz": [10000, 15000, 20000], "Filial Sul": [5000, 7000]}
+
+# função que recebe uma tupla e retorna o total de vendas e a média de vendas
+def analisar_vendas(lista_vendas):
+    total_vendas = sum(lista_vendas)
+    media_vendas = total_vendas / len(lista_vendas)
+
+    return total_vendas, media_vendas
+
+# Iterando sobre a lista de filiais
+for filial in dados_filiais:
+    total_vendas, media_vendas = analisar_vendas(dados_filiais[filial])
+    print(f'Filial {filial} -> Total: R${total_vendas:.2f}, Média: R${media_vendas:.2f}')   

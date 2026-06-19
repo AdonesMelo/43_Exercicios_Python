f52d67483934f6960c800e16d8b340e011c7a4ef
Adones Melo <adones.n.m@outlook.com>
2026-06-19 12:42:42 -0300

    Questão 1: Tuplas

---
+++ tuplas.py
@@ -0,0 +1,8 @@
+print('\n#######################################################################################\n')
+
+# 1
+coordenadas = (-23.5505, -46.6333) # tupla é imutável, ou seja, não pode ser alterada depois de criada
+
+latitude, longitude = coordenadas # desempacotamento de tupla
+
+print(f'Latitude: {latitude}, Longitude: {longitude}')
